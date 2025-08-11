090bd371c77636dc3f9f0d2df4bfc5b9b6aa423b
Alison Silva <alisongbsilva@gmail.com>
2025-08-11 07:22:06 -0300

    correção de rota '/'

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect()->route('pedidos.index');
+});
+
 Route::get('/pedidos', function () {
     return view('pedidos.index');
 })->name('pedidos.index');
